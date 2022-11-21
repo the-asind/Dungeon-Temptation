a5bdf2937f335d82a4164054115a38cbda3c2bd4
Danil <70006380+TheNorth322@users.noreply.github.com>
2022-11-21 18:21:43 +0700

    add movement, collision
    add top-down player movement and collision on walls and characters

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
@@ -9,41 +11,48 @@ using Vector3 = UnityEngine.Vector3;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
+    private Vector2 moveDelta;
+    private Vector2 target;
     private RaycastHit2D hit;
-
+    public float movementSpeed = 0.16f;
+    public float nextWalkTime = 0;
+    public float Cooldown = 0.05f;
+    Vector2 currentVelocity;
+    public float smoothTime = 0.1f;
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
+    private void Update()
+    {
+        moveDelta.x = Input.GetAxisRaw("Horizontal");
+        moveDelta.y = Input.GetAxisRaw("Vertical");
+    }
+
     private void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        moveDelta = new Vector3(x, y, 0);
-
         // Player rotation
         if (moveDelta.x > 0)
             transform.localScale = Vector3.one;
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
-        
-        // Y axis collideration
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y),
-            Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-        
+
         // X axis collidetation
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0),
-            Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, moveDelta.y),
+            Mathf.Abs(moveDelta.x * movementSpeed) + Mathf.Abs(moveDelta.y * movementSpeed), LayerMask.GetMask("Actor", "Blocking"));
+        
+        // Smooth tile walking. Currently doesn't work
+        target.x = moveDelta.x * movementSpeed;
+        //target.x -= target.x % movementSpeed
+        target.y = moveDelta.y * movementSpeed;
+        //target.y -= target.y % movementSpeed;
+        
+        if (hit.collider == null && Time.time > nextWalkTime)
         {
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+            //transform.position = Vector3.SmoothDamp(transform.position, target, ref currentVelocity, smoothTime);
+            transform.Translate(moveDelta.x * movementSpeed, moveDelta.y * movementSpeed, 0);
+            nextWalkTime = Time.time + Cooldown;
         }
     }
 }
