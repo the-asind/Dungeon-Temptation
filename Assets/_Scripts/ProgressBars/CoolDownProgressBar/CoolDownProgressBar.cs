21f176692ee163caf6d81f0f96c9fb8b7197bedd
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2022-11-24 03:18:05 +0700

    Small fixed
    Fixed classes names of progress bars

--- Assets/_Scripts/ProgressBars/CoolDownProgressBar/CoolDownProgressBar.cs
+++ Assets/_Scripts/ProgressBars/CoolDownProgressBar/CoolDownProgressBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ProgressBar : MonoBehaviour
+public class CoolDownProgressBar : MonoBehaviour
 {
     [SerializeField] private Image imgFiller;
 
