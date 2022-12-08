482238b80d00467aa9cab9942298c493d6854f50
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2022-12-08 23:17:08 +0700

    Inventory work a bit

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -13,6 +13,8 @@ public class Inventory
         AddItem(new Item { itemType = Item.ItemType.Sword, amount = 1 });
         AddItem(new Item { itemType = Item.ItemType.XP, amount = 1 });
         AddItem(new Item { itemType = Item.ItemType.Coins, amount = 1 });
+        AddItem(new Item { itemType = Item.ItemType.Shurikens, amount = 1 });
+        //AddItem(new Item { itemType = Item.ItemType.Bow, amount = 1 });
     }
 
     public void AddItem(Item item)
--- Assets/UI_Inventory.cs
+++ Assets/UI_Inventory.cs
@@ -10,9 +10,9 @@ public class UI_Inventory : MonoBehaviour
 
     private void Awake()
     {
+        new WaitForSeconds(5);
         itemSlotContainer = transform.Find("itemSlotContainer");
-        itemSlotTemplate = transform.Find("itemSlotTemplate");
-        Debug.Log("UI_InventoryAwake");
+        itemSlotTemplate = transform.Find("itemSlotContainer/itemSlotTemplate");
     }
 
     public void SetInventory(Inventory inventory)
@@ -24,26 +24,22 @@ public class UI_Inventory : MonoBehaviour
     private void RefreshInventoryItems()
     {
         Debug.Log(inventory.GetItemList().Count);
-        //Debug.Log("F");
         int x = 0;
-        int y = 0;
-        float itemSlotCellSize = 50f;
+        int y = 1;
+        float itemSlotCellSize = 60f;
+        //itemSlotTemplate.gameObject.SetActive(true);
         foreach (Item item in inventory.GetItemList())
         {
-            Debug.Log("??????????????");
-            //RectTransform itemSlotRectTransform = 
-            Instantiate(itemSlotTemplate);//.GetComponent<RectTransform>();
-            Debug.Log("*********");
-            //itemSlotRectTransform.gameObject.SetActive(true);
-            Debug.Log("----------");
-            //itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotTemplate).GetComponent<RectTransform>();
+            itemSlotRectTransform.gameObject.SetActive(true);
+            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
             x++;
-            Debug.Log("!!!!!!!!!!!");
             if (x > 4)
             {
                 x = 0;
                 y++;
             }
         }
+        //itemSlotTemplate.gameObject.SetActive(false);
     }
 }
