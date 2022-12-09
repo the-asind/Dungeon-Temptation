29ccaad1ba1a74cc4a7478b0735165ae75376232
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2022-12-09 23:37:41 +0700

    Revert "It works"
    This reverts commit 559380511bfdd731b1ef4cbfddabf8563bb77196.

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -14,7 +14,7 @@ public class Inventory
         AddItem(new Item { itemType = Item.ItemType.XP, amount = 1 });
         AddItem(new Item { itemType = Item.ItemType.Coins, amount = 1 });
         AddItem(new Item { itemType = Item.ItemType.Shurikens, amount = 1 });
-        AddItem(new Item { itemType = Item.ItemType.Bow, amount = 1 });
+        //AddItem(new Item { itemType = Item.ItemType.Bow, amount = 1 });
     }
 
     public void AddItem(Item item)
--- Assets/UI_Inventory.cs
+++ Assets/UI_Inventory.cs
@@ -25,12 +25,14 @@ public class UI_Inventory : MonoBehaviour
 
     private void RefreshInventoryItems()
     {
+        Debug.Log(inventory.GetItemList().Count);
         int x = 0;
-        int y = 0;
+        int y = 1;
         float itemSlotCellSize = 60f;
+        //itemSlotTemplate.gameObject.SetActive(true);
         foreach (Item item in inventory.GetItemList())
         {
-            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotTemplate).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
             itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
             
@@ -38,11 +40,12 @@ public class UI_Inventory : MonoBehaviour
             image.sprite = item.GetSprite();
 
             x++;
-            if (x > 3)
+            if (x > 4)
             {
                 x = 0;
-                y--;
+                y++;
             }
         }
+        //itemSlotTemplate.gameObject.SetActive(false);
     }
 }
